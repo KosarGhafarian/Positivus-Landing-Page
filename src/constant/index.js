ca581d4fb2052a5e60e1d9c9e7e147e52a77b3f2
Kothar-2000 <kotharghafarian2000@gmail.com>
2024-10-04 09:00:44 +0330

    Add Constant

---
+++ src/constant/index.js
@@ -0,0 +1,10 @@
+import searchEngine from "../assets/images/searchEngine.svg";
+
+export const ServiceCardData = [
+  {
+    id: 0,
+    title: "Search engine optimization",
+    image: searchEngine,
+    bgColour: "#F3F3F3",
+  },
+];
