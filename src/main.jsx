e757d28ae0b1cf4ad06b47ecac0a04340ef0f0bf
Kothar-2000 <kotharghafarian2000@gmail.com>
2024-12-05 15:14:43 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "@fontsource/space-grotesk";
 
@@ -13,9 +13,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
