1068ada2d3cc829953e93b4fdea6c31a9d24df67
Kothar-2000 <kotharghafarian2000@gmail.com>
2024-12-05 15:13:58 +0330

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import { Button, Grid2, Typography } from "@mui/material";
+import { Button, Grid2, Typography, Box } from "@mui/material";
 
+import MediaQuery from "../hooks/MediaQuery";
 import Illustration from "../assets/images/Illustration.svg";
 
 const Header = () => {
-  return (
-    <Grid2 container className="flex-nowrap" sx={{ paddingTop: "100px" }}>
-      <Grid2 container className="flex-column">
+  return MediaQuery() ? (
+    <Box className="header-box">
+      <Box>
         <Typography variant="h1" className="header-title">
           Navigating the digital landscape for success
         </Typography>
@@ -14,14 +15,31 @@ const Header = () => {
           through a range of services including SEO, PPC, social media
           marketing, and content creation.
         </Typography>
-        <Button className="header-button">Book a consultation</Button>
-      </Grid2>
-      <Grid2 container>
-        <Grid2>
-          <img src={Illustration} />
-        </Grid2>
-      </Grid2>
-    </Grid2>
+        <Button className="header-button" size="medium">
+          Book a consultation
+        </Button>
+      </Box>
+      <Box className="d-block">
+        <img src={Illustration} />
+      </Box>
+    </Box>
+  ) : (
+    <Box className="header-box">
+      <Typography variant="h1" className="header-title">
+        Navigating the digital landscape for success
+      </Typography>
+      <Box className="d-block" sx={{ width: "-webkit-fill-available" }}>
+        <img src={Illustration} />
+      </Box>
+      <Typography className="header-description">
+        Our digital marketing agency helps businesses grow and succeed online
+        through a range of services including SEO, PPC, social media marketing,
+        and content creation.
+      </Typography>
+      <Button className="header-button" size="medium">
+        Book a consultation
+      </Button>
+    </Box>
   );
 };
 
