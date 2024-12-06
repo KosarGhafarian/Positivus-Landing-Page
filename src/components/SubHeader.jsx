ef1dd984b886437d955dde751af69af20defe73e
Kothar-2000 <kotharghafarian2000@gmail.com>
2024-12-06 10:14:54 +0330

    Update SubHeader.jsx

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -1,11 +1,15 @@
 import { Grid2, Typography } from "@mui/material";
 
+import MediaQuery from "../hooks/MediaQuery";
+
 const SubHeader = ({ name, description, sx = {} }) => {
   return (
     <Grid2
       container
       sx={{ marginY: "100px !important" }}
-      className="gap-4 align-items-center"
+      className={`gap-4 align-items-center ${
+        MediaQuery() ? "" : "justify-content-center"
+      }`}
     >
       <Typography variant="h2" className="sub-header-name">
         {name}
