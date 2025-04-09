0025450e5a7d6bf1d1fa5c2dc3308c791a89f49d
Kothar-2000 <kotharghafarian2000@gmail.com>
2025-04-09 23:59:49 +0330

    Update Team.jsx

--- src/Routes/Team.jsx
+++ src/Routes/Team.jsx
@@ -1,16 +1,42 @@
-import { Grid2 } from "@mui/material";
+import { Box, Card, CardContent, Typography } from "@mui/material";
 
+import { TeamData } from "../constant/index";
 import SubHeader from "../components/SubHeader";
+
+import "../assets/styles/team.css";
 
 const Team = () => {
   return (
-    <Grid2>
+    <Box sx={{ paddingBottom: "100px", marginBottom: "100px" }}>
       <SubHeader
         name="Team"
-        sx={{ width: "473px" }}
+        sx={{ maxWidth: "473px !important", minWidth: "345px" }}
         description="Meet the skilled and experienced team behind our successful digital marketing strategies"
       />
-    </Grid2>
+      <Box className="team-detail">
+        {TeamData.map((data) => (
+          <Card variant="outlined" className="team-card" key={data.id}>
+            <CardContent className="team-personal">
+              <Box className="detail">
+                <Box className="profile-box">
+                  <Box
+                    className="profile-image"
+                    sx={{ backgroundImage: `url(${data.profile})` }}
+                  ></Box>
+                </Box>
+                <Box>
+                  <Typography>{data.name}</Typography>
+                  <Typography>{data.occupation}</Typography>
+                </Box>
+              </Box>
+              <Box className="description">
+                <Typography>{data.description}</Typography>
+              </Box>
+            </CardContent>
+          </Card>
+        ))}
+      </Box>
+    </Box>
   );
 };
 
