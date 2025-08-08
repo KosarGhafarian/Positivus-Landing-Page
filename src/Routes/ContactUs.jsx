9eef4a7b5c79bd7a67d1bfec9a24b7a9e0f33813
Kothar-2000 <kotharghafarian2000@gmail.com>
2025-08-08 19:39:13 +0330

    Create ContactUs.jsx

---
+++ src/Routes/ContactUs.jsx
@@ -0,0 +1,16 @@
+import { Box, Typography } from "@mui/material";
+
+import SubHeader from "../components/SubHeader";
+const ContactUs = () => {
+  return (
+    <Box id="contactus">
+      <SubHeader
+        name="Contact Us"
+        description="Connect with Us: Let's Discuss Your Digital Marketing Needs"
+        sx={{ width: "358px !important" }}
+      />
+    </Box>
+  );
+};
+
+export default ContactUs;
